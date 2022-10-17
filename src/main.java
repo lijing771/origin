1cef8c774caae7d312b32610f438a8e93e9b3519
lijing <1499072232@qq.com>
2022-10-17 20:40:33 +0800

    third version

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("version1");
         System.out.println("version2");
+        System.out.println("version3");
     }
 }
